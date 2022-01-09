0273c9ed8e6106eb8cd8d66be90c8da448f8e542
khelyorbek <khelyorbek@gmail.com>
2022-01-08 20:56:38 -0500

    Completed some and every exercises. All tests passed

---
+++ springboard/08_AdvancedArrayMethods/8_2_someAndEvery/some-every.js
@@ -0,0 +1,100 @@
+/*
+Write a function called hasOddNumber which accepts an array and returns true if the array contains at least one odd number, otherwise it returns false.
+
+Examples:
+    hasOddNumber([1,2,2,2,2,2,4]) // true
+    hasOddNumber([2,2,2,2,2,4]) // false
+*/
+
+function hasOddNumber(arr) {
+    return arr.some(function(num){
+        return num % 2 > 0;
+    })
+}
+
+// hasAZero
+// Write a function called hasAZero which accepts a number and returns true if that number contains at least one zero. Otherwise, the function should return false
+
+// hasAZero(33321232131012) // true
+// hasAZero(1212121) // false
+
+function hasAZero(num){
+    const arr = num.toString().split('');
+    return arr.some(function(n){
+        console.log(n);
+        return n == 0;
+    })
+
+}
+/*
+Write a function called hasOnlyOddNumbers which accepts an array and returns true if every single number in the array is odd. If any of the values in the array are not odd, the function should return false. 
+
+Examples:
+    hasOnlyOddNumbers([1,3,5,7]) // true
+    hasOnlyOddNumbers([1,2,3,5,7]) // false
+*/
+
+function hasOnlyOddNumbers(arr) {
+    return arr.every(function(num){
+        return num % 2 !== 0;
+    })
+}
+
+/*
+Write a function called hasNoDuplicates which accepts an array and returns true if there are no duplicate values (more than one element in the array that has the same value as another). If there are any duplicates, the function should return false.
+
+Examples:
+    hasNoDuplicates([1,2,3,1]) // false
+    hasNoDuplicates([1,2,3]) // true
+*/
+
+function hasNoDuplicates(arr) {
+    return arr.every(function(num){
+        return arr.indexOf(num) === arr.lastIndexOf(num);
+    })
+}
+
+
+
+/*
+Write a function called hasCertainKey which accepts an array of objects and a key, and returns true if every single object in the array contains that key. Otherwise it should return false.
+
+Examples:
+    var arr = [
+        {title: "Instructor", first: 'Elie', last:"Schoppik"}, 
+        {title: "Instructor", first: 'Tim', last:"Garcia", isCatOwner: true}, 
+        {title: "Instructor", first: 'Matt', last:"Lane"}, 
+        {title: "Instructor", first: 'Colt', last:"Steele", isCatOwner: true}
+    ]
+    
+    hasCertainKey(arr,'first') // true
+    hasCertainKey(arr,'isCatOwner') // false
+*/
+
+function hasCertainKey(objArr, key) {
+    return objArr.every(function(val){
+        return val[key];
+    })
+}
+
+/*
+Write a function called hasCertainValue which accepts an array of objects and a key, and a value, and returns true if every single object in the array contains that value for the specific key. Otherwise it should return false.
+
+Examples:
+    var arr = [
+        {title: "Instructor", first: 'Elie', last:"Schoppik"}, 
+        {title: "Instructor", first: 'Tim', last:"Garcia", isCatOwner: true}, 
+        {title: "Instructor", first: 'Matt', last:"Lane"}, 
+        {title: "Instructor", first: 'Colt', last:"Steele", isCatOwner: true}
+    ]
+    
+    hasCertainValue(arr,'title','Instructor') // true
+    hasCertainValue(arr,'first','Elie') // false
+    
+*/
+
+function hasCertainValue(objArr, key, searchValue) {
+    return objArr.every(function(val){
+        return val[key] === searchValue;
+    })
+}
