e94d04c9a40cefe7082da8e21ec2da860953d368
khelyorbek <khelyorbek@gmail.com>
2022-03-27 13:35:51 -0400

    24.5 Hashing and Auth Exercise - adding WTForms and templates related to user

---
+++ springboard/24_Intermediate_Flask/24_5_Hashing_and_Login/forms.py
@@ -0,0 +1,19 @@
+from ast import Pass
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, EmailField
+from wtforms.validators import InputRequired
+
+class UserForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
+    email = EmailField("Email", validators=[InputRequired()])
+    first_name = StringField("First Name", validators=[InputRequired()])
+    last_name = StringField("Last Name", validators=[InputRequired()])
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
+
+class FeedbackForm(FlaskForm):
+    title = StringField('Title', validators=[InputRequired()])
+    content = StringField('Notes', validators=[InputRequired()])
