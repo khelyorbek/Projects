06abe4eaf7dd0933a0eac6bb80967fb4e2950173
khelyorbek <khelyorbek@gmail.com>
2022-07-25 20:56:34 -0400

    36_1 - main app, config, custom error and server

---
+++ springboard/36_Intermediate_Express/36_1_Hashing_and_JSON_Web_Tokens/config.js
@@ -0,0 +1,20 @@
+/** Common config for message.ly */
+
+// read .env files and make environmental variables
+
+require("dotenv").config();
+
+const DB_URI = (process.env.NODE_ENV === "test")
+  ? "postgresql://test:test@localhost/messagely_test"
+  : "postgresql://test:test@localhost/messagely";
+
+const SECRET_KEY = process.env.SECRET_KEY || "secret";
+
+const BCRYPT_WORK_FACTOR = 12;
+
+
+module.exports = {
+  DB_URI,
+  SECRET_KEY,
+  BCRYPT_WORK_FACTOR,
+};
