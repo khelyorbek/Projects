14c9c3b7d3e088159a5ce0c0f5fc8ce3a060079c
khelyorbek <khelyorbek@gmail.com>
2022-07-25 20:55:33 -0400

    36_1 - SQL data file, db config and npm packages

---
+++ springboard/36_Intermediate_Express/36_1_Hashing_and_JSON_Web_Tokens/data.sql
@@ -0,0 +1,38 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS messages;
+
+CREATE TABLE users (
+    username text PRIMARY KEY,
+    password text NOT NULL,
+    first_name text NOT NULL,
+    last_name text NOT NULL,
+    phone text NOT NULL,
+    join_at timestamp without time zone NOT NULL,
+    last_login_at timestamp with time zone
+);
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    from_username text NOT NULL REFERENCES users,
+    to_username text NOT NULL REFERENCES users,
+    body text NOT NULL,
+    sent_at timestamp with time zone NOT NULL,
+    read_at timestamp with time zone
+);
+
+--          List of relations
+--  Schema |   Name   | Type  | Owner
+-- --------+----------+-------+-------
+--  public | messages | table | khely
+--  public | users    | table | khely
+-- (2 rows)
+
+-- messagely=# SELECT * FROM messages;
+--  id | from_username | to_username | body | sent_at | read_at
+-- ----+---------------+-------------+------+---------+---------
+-- (0 rows)
+
+-- messagely=# SELECT * FROM users;
+--  username | password | first_name | last_name | phone | join_at | last_login_at
+-- ----------+----------+------------+-----------+-------+---------+---------------
+-- (0 rows)
