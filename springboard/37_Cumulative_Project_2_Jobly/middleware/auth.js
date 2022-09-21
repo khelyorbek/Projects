671eba903ea63d0021595340de64965ca86308f7
khelyorbek <khelyorbek@gmail.com>
2022-09-21 17:00:01 -0400

    37 Jobly - middleware

---
+++ springboard/37_Cumulative_Project_2_Jobly/middleware/auth.js
@@ -0,0 +1,73 @@
+"use strict";
+
+/** Convenience middleware to handle common auth cases in routes. */
+// requiring JWT, secret key and our custom error methods
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config");
+const { UnauthorizedError } = require("../expressError");
+
+
+/** Middleware: Authenticate user.
+ *
+ * If a token was provided, verify it, and, if valid, store the token payload
+ * on res.locals (this will include the username and isAdmin field.)
+ *
+ * It's not an error if no token was provided or if the token is not valid.
+ */
+
+function authenticateJWT(req, res, next) {
+  try {
+    const authHeader = req.headers && req.headers.authorization;
+    if (authHeader) {
+      const token = authHeader.replace(/^[Bb]earer /, "").trim();
+      res.locals.user = jwt.verify(token, SECRET_KEY);
+    }
+    return next();
+  } catch (err) {
+    return next();
+  }
+}
+
+/** Middleware to use when they must be logged in.
+ *
+ * If not, raises Unauthorized.
+ */
+
+function ensureLoggedIn(req, res, next) {
+  try {
+    if (!res.locals.user) throw new UnauthorizedError();
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
+
+// Adding a custom method for checking if the user is an admin
+// Basically a copy of ensureLoggedIn with modified if check
+function ensureAdmin(req, res, next) {
+  try {
+    if (!res.locals.user || !res.locals.user.isAdmin) throw new UnauthorizedError();
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
+
+// Adding a custom method for checking if the user is an admin or if the user is a current user
+// Basically a copy of ensureAdmin with modified if check
+function ensureAdminOrSameUser(req, res, next) {
+  try {
+    const user = res.locals.user;
+    if (!user || !user.isAdmin && user.username !== req.params.username ) throw new UnauthorizedError();
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
+
+module.exports = {
+  authenticateJWT,
+  ensureLoggedIn,
+  ensureAdmin,
+  ensureAdminOrSameUser,
+};
