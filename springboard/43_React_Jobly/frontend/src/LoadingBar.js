e4da19050223193adc7e31e808115fcc56094aae
khelyorbek <khelyorbek@gmail.com>
2022-12-02 19:27:56 -0500

    43_react_jobly - part 8

--- springboard/43_React_Jobly/frontend/src/LoadingBar.js
+++ springboard/43_React_Jobly/frontend/src/LoadingBar.js
@@ -4,8 +4,11 @@ import Container from 'react-bootstrap/Container';
 
 const LoadingBar = () => {
     return  <>
+                {/* showing a bootstrap container */}
                 <Container className="text-center p-5">
+                    {/* using a bootstrap spinner component */}
                     <Spinner animation="grow" role="status" variant="info"> </Spinner>
+                    {/* showing just h1 with loading text */}
                     <h1 className="text-info">Loading</h1>
                 </Container>
             </>
