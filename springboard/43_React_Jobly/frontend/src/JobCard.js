bf9b59020ce177e49768e5f853ba373115c82936
khelyorbek <khelyorbek@gmail.com>
2022-12-09 22:14:53 -0500

    43_react_jobly - part 17

--- springboard/43_React_Jobly/frontend/src/JobCard.js
+++ springboard/43_React_Jobly/frontend/src/JobCard.js
@@ -1,9 +1,17 @@
-import React, { useState } from 'react';
+// react component
+import React, { useState, useContext, useEffect } from 'react';
 
+// images were breaking if used from the public folder so I had to import them
+// solution from: https://stackoverflow.com/questions/34582405/react-wont-load-local-images
+import imgApply from "./apply-light.png"
+import imgApplyDone from "./apply-success-light.png"
+import JoblyApi from './api';
+import GlobalContext from './GlobalContext';
 
 // Bootstrap
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Placeholder from 'react-bootstrap/Placeholder';
 
 // Custom css
 import "./CompanyCard.css";
@@ -13,16 +21,40 @@ import "./CompanyCard.css";
 
 // receiving the full data for each card as props
 const JobCard = (props) => {
-    const [applied, setApplied] = useState(false);
+    // destructuring the data param passed to us and storing everything into variables
+    const { id, title, salary, equity, companyName } = props.data;
 
-    const applyToJob = (e) => {
-        setApplied(!applied);
+    // adding state and toggle for data loaded or not
+    const [applied, setApplied] = useState(false);
+    const [dataLoaded, setDataLoaded] = useState(false)
+
+    // context for curr user
+    const { currentUser } = useContext(GlobalContext);
+    
+    // custom function to apply to a Job
+    async function applyToJob() {
+        // setting state of applied to true
+        setApplied(true);
+        // sending API call to add applied job into the database
+        await JoblyApi.applyToJob(currentUser.username, id);
+        // adding the current job into the currentUser context into application array
+        currentUser.applications.push(id);
     }
 
+    // when id or applications array changes, also runs when the card first renders
+    useEffect(() => {
+        // check if the current job id is listed in the applications array
+        if (currentUser.applications.includes(id)) {
+            // if so, set the applied of this card's state true
+            setApplied(true);
+        }
+        // set the data loading toggle to true so the data can display
+        setDataLoaded(true);
+    }, [id, currentUser.applications])
 
-    // destructuring the data param passed to us and storing everything into variables
-    const { id, title, salary, equity, companyHandle, companyName } = props.data;
-    return (<>
+    // showing this if the data is loaded
+    if (dataLoaded) {
+        return (<>
 
             {/* Creating a bootstrap Card component */}
             <Card className='g-0 my-2 cardFull'>
@@ -32,26 +64,71 @@ const JobCard = (props) => {
                 {/* Showing all the other data in the body of the card */}
                 <Card.Body>
                     {/* a button to apply for job with some basic styling */}
-                    {/* Also checking if logoUrl field exist in the data, if not, showing an image that says No logo */}
-                    <Button onClick={applyToJob} style={{float: 'right', padding:'1rem'}} className={applied ? "btn-success" : "btn-primary"}>
-                        <img alt="apply" src={applied ? "apply-success-light.png" : "apply-light.png"} style={{height: "1.7rem"}}></img> <strong>{applied ? "Applied" : "Apply"}</strong>
+                    {/* if the job has been applied, setting the icon, text to applied and disabling the button */}
+                    {/* if the job hasn't been applied, enable the button and set the text and icon to apply */}
+                    <Button
+                        disabled={applied ? true : ''}
+                        onClick={applyToJob}
+                        style={{ float: 'right', padding: '1rem' }}
+                        className={applied ? "btn-success" : "btn-primary"}
+                    >
+                        <img alt="apply" src={applied ? imgApplyDone : imgApply} style={{ height: "1.7rem" }}></img> <strong>{applied ? "Applied" : "Apply"}</strong>
                     </Button>
-                    
-                    <Card.Text> 
-                        <p>Salary: {salary}</p>
-                        <p>Equity: {equity ? equity : "None"}</p>
-                    
+
+                    {/* showing the salary and the equity for each job */}
+                    <Card.Text>
+                        <span>Salary: {salary ? salary : "Unknown"}</span>
+                        <br></br>
+                        <span>Equity: {equity ? equity : "None"}</span>
+
                     </Card.Text>
-                    {/* Button below can be used for other purposes in the future */}
-                    {/* <Button variant="primary">Go somewhere</Button> */}
+                    
                 </Card.Body>
                 <Card.Footer>
                     {/* Showing the amount of employees in the footer  */}
                     <Card.Subtitle>Company: {companyName}</Card.Subtitle>
                 </Card.Footer>
             </Card>
-    </>)
-    
+        </>)
+    } 
+    // if the data hasn't loaded yet, show this
+    else {
+        return (<>
+
+            {/* Creating a bootstrap Card component */}
+            <Card className='g-0 my-2 cardFull'>
+                {/* showing a placeholder that acts like a title */}
+                <Card.Header>
+                    <Placeholder as={Card.Title} animation="glow">
+                        <Placeholder xs={6} />
+                    </Placeholder>
+                </Card.Header>
+
+                {/* Showing all the other data in the body of the card */}
+                <Card.Body>
+                    {/* showing a placeholder that acts like a button */}
+                    <Placeholder.Button className="btn-primary" aria-hidden="true" style={{ float: 'right', padding: '1rem', color: 'transparent'}}> Applied___</Placeholder.Button>
+
+                    {/* showing a placeholder that acts like Card text */}
+                    <Placeholder as={Card.Text} animation="glow">
+                        <Placeholder xs={3} />
+                    </Placeholder>
+
+                    {/* showing a placeholder that acts like Card text */}
+                    <Placeholder as={Card.Text} animation="glow">
+                        <Placeholder xs={2} />
+                    </Placeholder>
+                </Card.Body>
+                <Card.Footer>
+                    {/* showing placeholder that acts like a Card subtitle in the footer */}
+                    <Placeholder as={Card.Subtitle} animation="glow">
+                        <Placeholder xs={4} />
+                    </Placeholder>
+                </Card.Footer>
+            </Card>
+        </>)
+    }
+
 }
 
 export default JobCard;
