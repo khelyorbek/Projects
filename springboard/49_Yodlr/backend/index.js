803dae4b6bb88cecf7d741bd74604d0d7ce187b6
khelyorbek <khelyorbek@gmail.com>
2022-12-26 17:24:00 -0500

    49_Yodlr_Backend - Part 3

---
+++ springboard/49_Yodlr/backend/index.js
@@ -0,0 +1,45 @@
+var debug = require('debug')('frontend-code-challenge');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var logger = require('./lib/logger');
+var cors = require('cors')
+
+var users = require('./routes/users');
+
+var app = express();
+var log = logger(app);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors()) // Use this after the variable declaration
+
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// development error handler
+// will print stacktrace
+app.use(function(err, req, res, next) {
+  log.error(err);
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
+app.set('port', process.env.PORT || 3001);
+
+var server = app.listen(app.get('port'), function() {
+  log.info('Express server listening on http://localhost:%d', server.address().port);
+});
