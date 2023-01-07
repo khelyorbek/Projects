68da2a4f04ce6adf5376c1b6b969d5e66a2ea71b
khelyorbek <khelyorbek@gmail.com>
2023-01-06 19:43:26 -0500

    49_Yodlr_Fronted - Part 8

---
+++ springboard/49_Yodlr/frontend/src/routes/Home.js
@@ -0,0 +1,17 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="App">
+    <header className="App-header">
+      <img src="logoipsum-293.svg" className="App-logo" alt="logo" />
+      <p>
+        <strong>Welcome to Yodlr!</strong>
+      </p>
+      <p>A place where things happen!</p>
+    </header>
+  </div>
+  );
+}
+
+export default Home;
